89334b64b6efc07623155d312630f4392badbf51
tanyaagrawalhii <nat.agra7@gmail.com>
2025-03-22 23:10:06 +0530

    Authorization

--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 // path and url help us to properly set paths when we configure directories.
 
