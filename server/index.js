5e6919c79437f10f31a9331e4d02cd43ae972f4f
tanyaagrawalhii <nat.agra7@gmail.com>
2025-03-22 21:08:56 +0530

    register

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 // path and url help us to properly set paths when we configure directories.
 
@@ -38,3 +39,26 @@ const storage = multer.diskStorage({
     },
   });
   const upload = multer({ storage });
+
+  /* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+
+  /* MONGOOSE SETUP */
+  const PORT = process.env.PORT || 6001;
+  mongoose
+    .connect(process.env.MONGO_URL) 
+    .then(() => {
+      app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+  
+      /* ADD DATA ONE TIME */
+      // User.insertMany(users);
+      // Post.insertMany(posts);
+    })
+    .catch((error) => console.error(`MongoDB connection error: ${error}`));
+  
