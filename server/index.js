3558314529dd26934540691ade64c7c9223b43bd
tanyaagrawalhii <nat.agra7@gmail.com>
2025-06-02 10:52:17 +0530

    backend created

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import {users , posts} from "./data/index.js";
 
 // path and url help us to properly set paths when we configure directories.
 
@@ -54,16 +57,16 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
-  /* MONGOOSE SETUP */
-  const PORT = process.env.PORT || 6001;
-  mongoose
-    .connect(process.env.MONGO_URL) 
-    .then(() => {
-      app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL) 
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
   
       /* ADD DATA ONE TIME */
       // User.insertMany(users);
       // Post.insertMany(posts);
-    })
-    .catch((error) => console.error(`MongoDB connection error: ${error}`));
+  })
+  .catch((error) => console.error(`MongoDB connection error: ${error}`));
   
